4186644609683f9b10ece290285f76475bfcf62f
Pavani Dasari <pdasari@innominds.com>
2019-06-23 23:58:42 +0530

    profile

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  @Input() title: any;
+  @Input() name: any;
+  @Output() logout = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  logoutEvent() {
+    this.logout.emit(null);
   }
 
 }
--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.sass']
+  styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  user: any;
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.user = JSON.parse(localStorage.getItem('userInfo'));
+    if (!this.user) {
+      this.router.navigateByUrl('/login');
+    }
+  }
+
+  logoutUser(temp) {
+    localStorage.clear();
+    this.router.navigateByUrl('/login');
   }
 
 }
